bfa79ae465069d119cf02c950f900a35a44d6ca3
DavidSanchezz2004 <davidaaronsanchez2004@gmail.com>
2026-04-24 14:38:15 -0500

    fix: agregar SpotDetraccionSeeder al DatabaseSeeder para produccion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Database\Seeders\InitialSetupSeeder;
+use Database\Seeders\SpotDetraccionSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             InitialSetupSeeder::class,
+            SpotDetraccionSeeder::class,
         ]);
     }
 }
